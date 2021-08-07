3cf8af45cae03ded42cf001dfd06ca1a03a52171
10xtarun <10xtarun@gmail.com>
2021-08-08 02:43:37 +0530

    material plugin added and configured

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
+    "gatsby-plugin-material-ui",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
